89736ef62ac227e717612ac7f9b7a99e163847cf
RickVerburg232906 <232906@buas.nl>
2024-06-10 12:20:16 +0200

    .

--- customer_a_n_w_b_app/customer_a_n_w_b_app/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.customeranwbapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- customer_a_n_w_b_app/customer_a_n_w_b_app/lib/index.dart
+++
@@ -1,3 +0,0 @@
-// Export pages
-export '/pages/app_page/app_page_widget.dart' show AppPageWidget;
-export '/pages/front_page/front_page_widget.dart' show FrontPageWidget;
